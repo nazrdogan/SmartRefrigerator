410e60133bab9c1212ca179a212e94619e9665c2
nazrdogan <nazrdogan@gmail.com>
2013-04-22 00:24:21 +0300

    yumurta yerinin tanımlası

---
+++ Smart/Smart.ino
@@ -0,0 +1,77 @@
+//Smart Refrigerator Project Arduino Code
+
+// For Egg box
+int egg1=22;
+int egg2=23;
+int egg3=24;
+int egg4=25;
+int egg5=26;
+int egg6=27;
+int egg7=28;
+int egg8=29;
+int egg9=30;
+int egg10=31;
+int egg11=32;
+int egg12=33;
+int egg13=34;
+int egg14=35;
+int egg15=36;
+int vaL1=0;
+int vaL2=0;
+int vaL3=0;
+int vaL4=0;
+int vaL5=0;
+int vaL6=0;
+int vaL7=0;
+int vaL8=0;
+int vaL9=0;
+int vaL10=0;
+int vaL11=0;
+int vaL12=0;
+int vaL13=0;
+int vaL14=0;
+int vaL15=0;
+//For 
+
+void setup()
+{
+  pinMode(egg1,INPUT);
+  pinMode(egg2,INPUT);
+  pinMode(egg3,INPUT);
+  pinMode(egg4,INPUT);
+  pinMode(egg5,INPUT);
+  pinMode(egg6,INPUT);
+  pinMode(egg7,INPUT);
+  pinMode(egg8,INPUT);
+  pinMode(egg9,INPUT);
+  pinMode(egg10,INPUT);
+  pinMode(egg11,INPUT);
+  pinMode(egg12,INPUT);
+  pinMode(egg13,INPUT);
+  pinMode(egg14,INPUT);
+  pinMode(egg15,INPUT);
+  
+  
+}
+void loop()
+{
+  vaL1=digitalRead(egg1);
+  vaL2=digitalRead(egg2);
+  vaL3=digitalRead(egg3);
+  vaL4=digitalRead(egg4);
+  vaL5=digitalRead(egg5);
+  vaL6=digitalRead(egg6);
+  vaL7=digitalRead(egg7);
+  vaL8=digitalRead(egg8);
+  vaL9=digitalRead(egg9);
+  vaL10=digitalRead(egg10);
+  vaL11=digitalRead(egg11);
+  vaL12=digitalRead(egg12);
+  vaL13=digitalRead(egg13);
+  vaL14=digitalRead(egg14);
+  vaL15=digitalRead(egg15);
+ 
+  
+  
+  
+}
