09eff07fd3a200bf1783df3beb5f73a0712939e7
nazrdogan <nazrdogan@gmail.com>
2013-04-22 21:20:15 +0300

    diger eşyalar

--- Smart/Smart.ino
+++ Smart/Smart.ino
@@ -1,3 +1,18 @@
+#define trigPin 12
+#define echoPin 13
+//--------
+#define trigPin1 11
+#define echoPin1 10
+//--------------
+#define trigPin2 9
+#define echoPin2 8
+//--------------
+#define trigPin3 7
+#define echoPin3 6
+
+
+
+
 //Smart Refrigerator Project Arduino Code
 
 // For Egg box
@@ -54,6 +69,21 @@ int eggSum;
 
 void setup()
 {
+   Serial.begin (9600);
+  //--------------------------
+   pinMode(trigPin, OUTPUT);
+   pinMode(echoPin, INPUT);
+   //---------------------------
+   pinMode(trigPin1, OUTPUT);
+   pinMode(echoPin1, INPUT);
+   //---------------------------
+   pinMode(trigPin2, OUTPUT);
+   pinMode(echoPin2, INPUT);
+   //---------------------------
+   pinMode(trigPin3, OUTPUT);
+   pinMode(echoPin3, INPUT);
+  
+   
   pinMode(egg1,INPUT);
   pinMode(egg2,INPUT);
   pinMode(egg3,INPUT);
@@ -74,6 +104,48 @@ void setup()
 }
 void loop()
 {
+  int duration, distance;
+  int duration1,distance1;
+  int duration2,distance2;
+  int duration3,distance3;
+  digitalWrite(trigPin, HIGH);
+  digitalWrite(trigPin1,HIGH);
+  digitalWrite(trigPin2,HIGH);
+  digitalWrite(trigPin3,HIGH);
+  //----------------------------------
+  delayMicroseconds(1000);
+  digitalWrite(trigPin, LOW);
+  digitalWrite(trigPin1,LOW);
+  digitalWrite(trigPin2,LOW);
+  digitalWrite(trigPin3,LOW);
+  //---------------------------------
+  duration = pulseIn(echoPin, HIGH);
+  duration1=pulseIn(echoPin1,HIGH);
+  duration2=pulseIn(echoPin2,HIGH);
+  duration=pulseIn(echoPin3,HIGH);
+  distance = (duration/2) / 29.1;
+  distance1=(duration1/2)/29.1;
+  distance2=(duration2/2)/29.1;
+  distance3=(duration3/3)/29.1;
+  
+    Serial.print(distance);
+    Serial.println(" cm");
+    Serial.print(distance1);
+    Serial.println(" cm");
+    Serial.print(distance2);
+    Serial.println(" cm");
+    Serial.print(distance2);
+    Serial.println(" cm");
+    Serial.print(distance3);
+    Serial.println(" cm");
+     
+    
+  
+  delay(500);
+  
+  
+  
+  
   vaL1=digitalRead(egg1);
   vaL2=digitalRead(egg2);
   vaL3=digitalRead(egg3);
